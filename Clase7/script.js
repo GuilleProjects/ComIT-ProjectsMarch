1ee73c47f31fe15bd1bb0caf23c5ee07d258098b
Guillermina Julieta Gatti Bosoms <128055629+ProyectosGuillermina@users.noreply.github.com>
2023-04-04 12:44:11 -0300

    Add files via upload

---
+++ Clase7/script.js
@@ -0,0 +1,24 @@
+//console.log("baby");
+// alert("baby");
+
+/*
+var nombre="Maria";
+console.log(nombre); */
+
+
+var num1=8;
+var num2=5;
+var resta=0;
+
+resta=num1-num2;
+console.log(resta);
+
+/* 
+var num1=prompt();
+var num2=prompt();
+var resta=0;
+
+
+resta=num1-num2;
+console.log(resta);
+*/
